cfcade28009164e4c8ec5328cfc22fde60eb8b76
Amit Raj <navodaya2152@gmail.com>
2026-09-14 12:12:52 +0530

    remove admin panel completely

--- admin/js/config.js
+++
@@ -1,3 +0,0 @@
-// Paste the Firebase web configuration from Project settings > Your apps here.
-// The admin remains in demo mode until firebaseConfig.apiKey is set.
-window.ADMIN_CONFIG={firebaseConfig:{apiKey:"AIzaSyC1cd5SxWJ8r4BB0205GGPyoIxV8pC7BwA",authDomain:"vishwakarma-enterprises-138c8.firebaseapp.com",projectId:"vishwakarma-enterprises-138c8",storageBucket:"vishwakarma-enterprises-138c8.firebasestorage.app",messagingSenderId:"562693661621",appId:"1:562693661621:web:308d7fdd94afb0146f6ffc",measurementId:"G-6X6YZ334GV"},stats:{products:1000,categories:4,offers:0,reviews:3}};
